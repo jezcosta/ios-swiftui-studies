178bd3c0857b0b61eadd7409f88ad24641132b22
Jéssica Costa <jessicacosta.dev@hotmail.com>
2026-02-21 09:38:46 -0300

    Initial Commit

---
+++ AppMusica/AppMusicaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppMusicaApp.swift
+//  AppMusica
+//
+//  Created by Jessica Costa on 21/02/26.
+//
+
+import SwiftUI
+
+@main
+struct AppMusicaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
